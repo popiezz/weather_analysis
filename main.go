ded001b0574f2e32e325c6ecfe18486a0f01abfe
popiezz <philpopescu@outlook.com>
2025-07-07 11:40:00 -0400

    update

--- main.go
+++ main.go
@@ -2,179 +2,140 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-// the structure that is sent to supabase
-type weatherData struct {
-	Timestamp   string  `json:"timestamp"`
-	Location    string  `json:"location"`
-	Temperature float64 `json:"temperature"`
-	Humidity    float64 `json:"humidity"`
-	Wind_speed  float64 `json:"wind_speed"`
-	Conditions  string  `json:"conditions"`
-	Wind_degree float64 `json:"wind_degree"`
-	Wind_dir    string  `json:"wind_dir"`
-	Pressure_mb float64 `json:"pressure_mb"`
-	Cloud       float64 `json:"cloud"`
-	Feelslike_c float64 `json:"feelslike_c"`
-	Vis_km      float64 `json:"vis_km"`
-	Uv          float64 `json:"uv"`
-	Is_day      float64 `json:"is_day"`
+var baseURL = "http://api.weatherapi.com/v1/"
+
+type SupabaseData struct {
+	Location       string
+	Temperature    float64
+	Humidity       float64
+	IsDay          int8
+	Wind_speed     float64
+	Wind_direction string
+	UV             float64
+	Cloud          float64
 }
 
-// the structure that is received from weatherapi
-type WeatherAPIResponse struct {
+type WeatherAPIData struct {
 	Location struct {
 		Name string `json:"name"`
 	} `json:"location"`
 	Current struct {
-		TempC     float64 `json:"temp_c"`
-		Humidity  float64 `json:"humidity"`
-		WindKph   float64 `json:"wind_kph"`
-		Condition struct {
-			Text string `json:"text"`
-		} `json:"condition"`
-		LastUpdated string  `json:"last_updated"`
-		Wind_degree float64 `json:"wind_degree"`
-		Wind_dir    string  `json:"wind_dir"`
-		Pressure_mb float64 `json:"pressure_mb"`
-		Cloud       float64 `json:"cloud"`
-		Feelslike_c float64 `json:"feelslike_c"`
-		Vis_km      float64 `json:"vis_km"`
-		Uv          float64 `json:"uv"`
+		Temperature    float64 `json:"temp_c"`
+		Humidity       float64 `json:"humidity"`
+		IsDay          int8    `json:"is_day"`
+		Wind_speed     float64 `json:"wind_kph"`
+		Wind_direction string  `json:"wind_dir"`
+		Uv             float64 `json:"uv"`
+		Cloud          float64 `json:"cloud"`
 	} `json:"current"`
 }
 
-func sendWeatherDataCore(apiKey, supabaseURL, weatherApiKey, weatherURL string) error {
-	weatherResp, bodyBytes, err := getWeatherData(weatherApiKey, weatherURL)
+func getWeatherData(apiKey, location, baseURL string) (WeatherAPIData, []byte, error) {
+	url := fmt.Sprintf("%s/current.json?key=%s&q=%s", baseURL, apiKey, location)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return fmt.Errorf("failed to get weather data: %w", err)
+		slog.Error("failed creating request", slog.Any("err", err))
+		return WeatherAPIData{}, nil, err
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		slog.Error("client failed getting request", slog.Any("err", err))
+		return WeatherAPIData{}, nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		slog.Error("failed reading the response body", slog.Any("err ", err))
+		return WeatherAPIData{}, nil, err
+	}
+	var weatherResponse WeatherAPIData
+	err = json.Unmarshal(body, &weatherResponse)
+	if err != nil {
+		slog.Error("failed formatting the reponse into JSON", slog.Any("err ", err))
+		return WeatherAPIData{}, nil, err
+	}
+	return weatherResponse, body, err
+}
+
+func formatWeatherData(apiResp WeatherAPIData) SupabaseData {
+	return SupabaseData{
+		Location:       apiResp.Location.Name,
+		Temperature:    apiResp.Current.Temperature,
+		Humidity:       apiResp.Current.Humidity,
+		IsDay:          apiResp.Current.IsDay,
+		Wind_speed:     apiResp.Current.Wind_speed,
+		Wind_direction: apiResp.Current.Wind_direction,
+		UV:             apiResp.Current.Uv,
+		Cloud:          apiResp.Current.Cloud,
+	}
+}
+
+func sendDataSupabase(apiKey, url string, formattedData SupabaseData) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	if cancel != nil {
+		slog.Error("POST connection timmed out", slog.Any("err", cancel))
+	}
+	data, err := json.Marshal(formattedData)
+	if err != nil {
+		slog.Error("error marshalling the formatted weather data", slog.Any("err", err))
 	}
 
-	formatted := formatWeatherForSupabase(weatherResp)
-	// weather := []weatherData{formatted}
-
-	data, err := json.Marshal(formatted)
+	supaBaseURL := url + "/rest/v1/weather_data?select=*"
+	req, err := http.NewRequestWithContext(ctx, "POST", supaBaseURL, bytes.NewReader(data))
 	if err != nil {
-		return fmt.Errorf("failed to marshal weather data: %w", err)
+		slog.Error("Request error", slog.Any("err", err))
 	}
 
-	url := supabaseURL + "/rest/v1/weather_data?select=*"
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
-	if err != nil {
-		return fmt.Errorf("failed to create Supabase request: %w", err)
+	req.Header = http.Header{
+		"apikey":        []string{apiKey},
+		"Authorizaiton": []string{"Bearer " + apiKey},
+		"Content-Type":  []string{"application/json"},
+		"Prefer":        []string{"return=representation"},
 	}
-
-	req.Header.Set("apikey", apiKey)
-	req.Header.Set("Authorization", "Bearer "+apiKey)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Prefer", "return=representation")
-
 	log.Println("Final JSON payload:", string(data))
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("failed to send request to Supabase: %w", err)
+		slog.Error("POST response failed", slog.Any("err", err))
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return fmt.Errorf("supabase returned status %d: %s", resp.StatusCode, string(bodyBytes))
-	}
 
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("supabase returned status %d", resp.StatusCode)
+	}
 	return nil
 }
 
-func sendWeatherData(apiKey, supabaseURL, weatherApiKey, weatherURL string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		err := sendWeatherDataCore(apiKey, supabaseURL, weatherApiKey, weatherURL)
-		if err != nil {
-			c.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"message": "Weather data sent succesfully"})
-	}
-}
-
-func getWeatherData(apiKey, weatherURL string) (WeatherAPIResponse, []byte, error) {
-	url := fmt.Sprintf("%s/current.json?key=%s&q=Montreal", weatherURL, apiKey)
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return WeatherAPIResponse{}, nil, err
-	}
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return WeatherAPIResponse{}, nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return WeatherAPIResponse{}, nil, err
-	}
-	var weatherResp WeatherAPIResponse
-	err = json.Unmarshal(body, &weatherResp)
-	if err != nil {
-		return WeatherAPIResponse{}, nil, err
-	}
-	return weatherResp, body, nil
-}
-
-func formatWeatherForSupabase(apiResp WeatherAPIResponse) weatherData {
-	return weatherData{
-		Timestamp:   time.Now().UTC().Format(time.RFC3339),
-		Location:    apiResp.Location.Name,
-		Temperature: apiResp.Current.TempC,
-		Humidity:    apiResp.Current.Humidity,
-		Wind_speed:  apiResp.Current.WindKph,
-		Conditions:  apiResp.Current.Condition.Text,
-		Wind_degree: apiResp.Current.Wind_degree,
-		Wind_dir:    apiResp.Current.Wind_dir,
-		Pressure_mb: apiResp.Current.Pressure_mb,
-		Cloud:       apiResp.Current.Cloud,
-		Feelslike_c: apiResp.Current.Feelslike_c,
-		Vis_km:      apiResp.Current.Vis_km,
-		Uv:          apiResp.Current.Uv,
-	}
-}
-
 func main() {
-	// LOAD .env FILES
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		slog.Error("failed loading .env file", slog.Any("err ", err))
 	}
-	api_key := os.Getenv("SUPABASE_API_KEY")
-	supabaseURL := os.Getenv("SUPABASE_URL")
-	weather_api := os.Getenv("WEATHER_API_KEY")
-	weatherURL := os.Getenv("WEATHER_URL")
-
-	// CREATE TICKER AND ALL ITS DATA
-	interval := 15 * time.Minute
-	ticker := time.NewTicker(interval)
-	go func() {
-		for range ticker.C {
-			err := sendWeatherDataCore(api_key, supabaseURL, weather_api, weatherURL)
-			if err != nil {
-				log.Println("Ticker error:", err)
-			} else {
-				log.Println("Weather data sent successfully via ticker")
-			}
-		}
-	}()
-
-	router := gin.Default()
-	router.POST("/update", sendWeatherData(api_key, supabaseURL, weather_api, weatherURL))
-	router.Run("localhost:8080")
+	weather, _, err := getWeatherData(os.Getenv("WEATHER_API_KEY"), "Montreal", os.Getenv("WEATHER_URL"))
+	if err != nil {
+		slog.Error("failed getting data", slog.Any("err ", err))
+		return
+	}
+	formattedData := formatWeatherData(weather)
+	err = sendDataSupabase(os.Getenv("SUPABASE_API"), os.Getenv("SUPABASE_URL"), formattedData)
+	if err != nil {
+		slog.Error("Supabase insertion failed", slog.Any("err", err))
+		return
+	}
+	fmt.Println("Montreal weather is :", weather)
 }
